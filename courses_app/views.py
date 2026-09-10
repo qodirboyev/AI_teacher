c1b9a6e77f47638a3d1c554816000ea59953e431
DEMIR <qadirboevdemir@gmail.com>
2026-09-10 19:05:27 +0500

    API and gemini AI errors try/except

--- courses_app/views.py
+++ courses_app/views.py
@@ -2,6 +2,7 @@ from multiprocessing import context
 from google import genai
 from django.shortcuts import render
 import os
+from google.genai import errors
 
 
 from courses_app.models import Courses,Lessons
@@ -40,24 +41,34 @@ def lesson_chat_view(request):
         savol = request.POST.get("savol")
 
         client = genai.Client(api_key=os.getenv("API_KEY"))
+        try:
+            response = client.models.generate_content(
+                model="gemini-3.6-flash",
+                contents=f"""
+                Sen AI Teacher san senga savollar boladi sen dars otasan lekin 
+                hardoyim buni takiroroy aytishing shar 
+                emas savolga qisqa javo bilan 
+                darsni tushuntirishing kerak halos
+                agar savol bolmasa shunchaki darsga tayyor misiz deya javob qaytar
+    
+                Kurs: {course.name}
+    
+                O'quvchining savoli:
+                {savol}
+                """,
+            )
 
-        response = client.models.generate_content(
-            model="gemini-3.6-flash",
-            contents=f"""
-            Sen AI Teacher san senga savollar boladi sen dars otasan lekin 
-            hardoyim buni takiroroy aytishing shar 
-            emas savolga qisqa javo bilan 
-            darsni tushuntirishing kerak halos
-            agar savol bolmasa shunchaki darsga tayyor misiz deya javob qaytar
+            context["response"] = f"AI Teacher: {response.text}"
 
-            Kurs: {course.name}
+        except errors.ClientError as e:
 
-            O'quvchining savoli:
-            {savol}
-            """,
-        )
+            if e.code == 429:
+                context["response"] = "🤖 AI Teacher hozircha band. \nBirozdan keyin yana urinib ko‘ring."
+            else:
+                context["response"] = "❌ AI Teacher bilan bog‘lanishda xatolik yuz berdi."
 
-        context["response"] = f"AI Teacher: {response.text}"
+        except errors.ServerError:
+            context["response"] = "🤖 AI Teacher serveri hozir band. \nBirozdan keyin yana urinib ko‘ring."
 
     return render(request, template_name, context)
 
